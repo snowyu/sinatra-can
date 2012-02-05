9f035881116978ec58a790978520cf54c309b94d
Silvio Henrique Ferreira <shferreira@me.com>
2012-02-05 02:53:17 -0200

    Add 'finalize' to the tests, because DataMapper requires it now

--- spec/can_dm_spec.rb
+++ spec/can_dm_spec.rb
@@ -12,20 +12,6 @@ describe 'sinatra-can' do
   end
 
   before do
-    class User
-      def initialize(name = "guest")
-        @name = name
-      end
-
-      def name
-        @name
-      end
-
-      def is_admin?
-        @name == "admin"
-      end
-    end
-
     DataMapper.setup(:default, :adapter => 'in_memory')
 
     class Article
@@ -33,6 +19,17 @@ describe 'sinatra-can' do
 
       property :id, Serial
       property :title, String
+    end
+
+    class User
+      include DataMapper::Resource
+
+      property :id, Serial
+      property :name, String
+
+      def is_admin?
+        @name == "admin"
+      end
     end
 
     DataMapper.finalize
@@ -47,66 +44,69 @@ describe 'sinatra-can' do
     app.set :dump_errors, true
     app.set :raise_errors, true
     app.set :show_exceptions, false
+
+    User.create(:name => 'admin')
+    User.create(:name => 'guest')
   end
 
   it "should allow management to the admin user" do
-    app.user { User.new('admin') }
+    app.user { User.get(1) }
     app.get('/1') { can?(:edit, :all).to_s }
     get '/1'
     last_response.body.should == 'true'
   end
 
   it "shouldn't allow management to the guest" do
-    app.user { User.new('guest') }
+    app.user { User.get(2) }
     app.get('/2') { cannot?(:edit, :all).to_s }
     get '/2'
     last_response.body.should == 'true'
   end
 
   it "should act naturally when authorized" do
-    app.user { User.new('admin') }
+    app.user { User.get(1) }
     app.get('/3') { authorize!(:edit, :all); 'okay' }
     get '/3'
     last_response.body.should == 'okay'
   end
 
   it "should raise errors when not authorized" do
-    app.user { User.new('guest') }
+    app.user { User.get(2) }
     app.get('/4') { authorize!(:edit, :all); 'okay' }
     get '/4'
     last_response.status.should == 403
   end
 
   it "should respect the 'user' block" do
-    app.user { User.new('testing') }
+    app.user { User.create(:name => 'testing') }
     app.get('/5') { current_user.name }
     get '/5'
     last_response.body.should == "testing"
   end
 
   it "shouldn't allow a rule if it's not declared" do
-    app.user { User.new('admin') }
+    app.user { User.get(1) }
     app.get('/6') { can?(:destroy, :all).to_s }
     get '/6'
     last_response.body.should == "false"
   end
 
   it "should throw 403 errors upon failed conditions" do
-    app.user { User.new('admin') }
+    app.user { User.get(1) }
     app.get('/7', :can => [ :create, User ]) { 'ok' }
     get '/7'
     last_response.status.should == 403
   end
 
   it "should accept conditions" do
-    app.user { User.new('admin') }
+    app.user { User.get(1) }
     app.get('/8', :can => [ :edit, :all ]) { 'ok' }
     get '/8'
     last_response.status.should == 200
   end
 
   it "should accept not_auth and redirect when not authorized" do
-    app.user { User.new('guest') }
+    app.user { User.get(2) }
     app.get('/login') { 'login here' }
     app.get('/9') { authorize! :manage, :all, :not_auth => '/login'  }
     get '/9'
@@ -117,7 +117,7 @@ describe 'sinatra-can' do
   it "should autoload and autorize the model" do
     article = Article.create(:title => 'test1')
 
-    app.user { User.new('admin') }
+    app.user { User.get(1) }
     app.get('/10/:id') { load_and_authorize!(Article); @article.title }
     get '/10/' + article.id.to_s
     last_response.body.should == article.title
@@ -126,7 +126,7 @@ describe 'sinatra-can' do
   it "should shouldn't allow creation of the model" do
     article = Article.create(:title => 'test2')
 
-    app.user { User.new('admin') }
+    app.user { User.get(1) }
     app.post('/11', :model => ::Article) { }
     post '/11'
     last_response.status.should == 403
@@ -135,7 +135,7 @@ describe 'sinatra-can' do
   it "should autoload and autorize the model when using the condition" do
     article = Article.create(:title => 'test3')
 
-    app.user { User.new('admin') }
+    app.user { User.get(1) }
     app.get('/12/:id', :model => ::Article) { @article.title }
     get '/12/' + article.id.to_s
     last_response.body.should == article.title
@@ -144,7 +144,7 @@ describe 'sinatra-can' do
   it "should autoload when using the before do...end block" do
     article = Article.create(:title => 'test4')
 
-    app.user { User.new('admin') }
+    app.user { User.get(1) }
     app.before('/13/:id', :model => Article) { }
     app.get('/13/:id') { @article.title }
     get '/13/' + (article.id).to_s
@@ -152,7 +152,7 @@ describe 'sinatra-can' do
   end
 
   it "should return a 404 when the autoload fails" do
-    app.user { User.new('admin') }
+    app.user { User.get(1) }
     app.get('/article14/:id', :model => Article) { @article.title }
     get '/article14/999'
     last_response.status.should == 404
